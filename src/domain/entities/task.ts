596e60e630d3fe5828c6bf06e0e8d6727f709b3d
ViniciusTFM <viniciusos.2006@gmail.com>
2025-08-20 20:03:03 -0300

    Implementa entidades

--- src/domain/entities/task.ts
+++ src/domain/entities/task.ts
@@ -1,11 +1,10 @@
 import { IProject } from '../interfaces/project.interface';
 import { ITask } from '../interfaces/task.interface';
 import { IUser } from '../interfaces/user.interface';
-
-export class Project implements IProject {
+export class Task implements ITask {
 	id: number;
 	name: string;
-	description: string;
-	tasks: ITask[];
+	status: 'pending' | 'completed';
+	project: IProject;
 	user: IUser;
 }
